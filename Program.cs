a60559e3990b6c092d92a0f6ccd5f4d2ac015208
SanketV <sankethvivek@gmail.com>
2021-09-20 17:53:35 +0530

    binary_serialization

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            serial_dese.Serialize();
+
+        }
+    }
+}
---
+++ serial_dese.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace Serial
+{
+    class serial_dese
+    {
+
+        public static void Serialize()
+        {
+            Person p = new Person();
+            p.City = new City();
+            p.Name = "raj";
+            p.Age = 22;
+            p.City.Name = "Bangalore";
+            p.City.Population = 12131234;
+            var b = new BinaryFormatter();
+            Stream fs = new FileStream(@"C:\Training\EuroTraining\Files\newnewxml.txt", FileMode.Create, FileAccess.Write);
+            //var serializer = new XmlSerializer(typeof(Person));
+            //serializer.Serialize(fs, p);
+            b.Serialize(fs, p);
+            fs.Close();
+            Console.WriteLine("object serialized");
+            fs = new FileStream(@"C:\Training\EuroTraining\Files\newnewxml.txt", FileMode.Open, FileAccess.Read);
+            //Person p1 = (Person)serializer.Deserialize(fs);
+            Person p1 = (Person)b.Deserialize(fs);
+            Console.WriteLine("deserialized object");
+            Console.WriteLine(p1.Name);
+            Console.WriteLine(p1.Age);
+            Console.WriteLine(p1.City.Name);
+            Console.WriteLine(p1.City.Population);
+            //Console.WriteLine("deserialized object");
+            //Console.WriteLine(p1.Name);
+            //Console.WriteLine(p1.Age);
+            //Console.WriteLine(p1.City.Name);
+            //Console.WriteLine(p1.City.Population);
+
+        }
+
+
+
+
+        [Serializable]
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public City City { get; set; }
+
+            public override string ToString()
+            {
+                StringBuilder str = new StringBuilder();
+
+                str.AppendLine("Name: " + Name);
+                str.AppendLine("Age: " + Age);
+                str.AppendLine("City: " + City.Name);
+
+                return str.ToString();
+            }
+        }
+
+        [Serializable]
+        public class City
+        {
+            public string Name { get; set; }
+            public int Population { get; set; }
+        }
+    }
+}
+
